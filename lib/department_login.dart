2edd78c9e3ec1a1166c71e8c3ee2d651e718b344
Pooviga S <spooviga01@gmail.com>
2021-12-21 12:36:36 +0530

    sample

---
+++ lib/department_login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dept_login extends StatefulWidget {
+  const Dept_login({Key? key}) : super(key: key);
+
+  @override
+  _Dept_loginState createState() => _Dept_loginState();
+}
+
+class _Dept_loginState extends State<Dept_login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
