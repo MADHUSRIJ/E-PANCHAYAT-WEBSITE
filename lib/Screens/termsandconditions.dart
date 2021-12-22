7cd53d009c8317088ebc0a36db9a5a820916ba13
Jookate0726 <69692093+Jookate0726@users.noreply.github.com>
2021-12-22 21:30:02 +0530

    terms and conditions

--- lib/Screens/termsandconditions.dart
+++ lib/Screens/termsandconditions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../sizeconfig.dart';
+
 class TerAndCon extends StatefulWidget {
   const TerAndCon({Key? key}) : super(key: key);
 
@@ -10,11 +12,179 @@ class TerAndCon extends StatefulWidget {
 class _TerAndConState extends State<TerAndCon> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
-          body: Center(
-            child: Text("Terms and Conditions"),
+      backgroundColor: Colors.white,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(3),
+            border: Border.all(width: 0.2, color: Colors.grey)),
+        margin: EdgeInsets.symmetric(
+            horizontal: SizeConfig.width! * 8,
+            vertical: SizeConfig.height! * 2),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 1),
+                child: Text(
+                  "TERMS & CONDITIONS",
+                  style: TextStyle(
+                      color: Color(0xff223E98),
+                      fontWeight: FontWeight.bold,
+                      fontSize: SizeConfig.size! * 20,
+                      letterSpacing: 0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "1. TERMS : ",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: SizeConfig.size! * 15,
+                      letterSpacing: 0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "2. USE LICENSE : ",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: SizeConfig.size! * 15,
+                      letterSpacing: 0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       a. Permission is granted to temporarily download one copy of the materials (information or software) on Prematix Software Solution's web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       i. Modify or copy the materials",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       ii. Use the materials for any commercial purpose, or for any public display (commercial or non-commercial)",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       iii. Attempt to decompile or reverse engineer any software contained on Prematix Software Solution's web site",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       iv. Remove any copyright or other proprietary notations from the materials",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       v. Transfer the materials to another person or 'mirror' the materials on any other server.",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: SizeConfig.width! * 3),
+                child: Text(
+                  "       b. This license shall automatically terminate if you violate any of these restrictions and may be terminated by Prematix Software Solution at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: SizeConfig.size! * 13,
+                      letterSpacing: 0.6),
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    ));
   }
 }
--- lib/department_login.dart
+++ lib/department_login.dart
@@ -16,33 +16,30 @@ class _Dept_loginState extends State<Dept_login> {
     return SafeArea(
         child: Scaffold(
             backgroundColor: Colors.white,
-          body:Center(
-            child:Row(
-              children: [
+            body: Center(
+              child: Row(children: [
                 Expanded(
-                  flex:2,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(
-                        vertical: SizeConfig.height! * 18,
-                        horizontal: SizeConfig.width! * 3),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1,
-                          color: Colors.grey.shade200,
-                        )),
-                  )
-                ),
-                Expanded(
+                    flex: 2,
                     child: Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical: SizeConfig.height! * 18,
+                          horizontal: SizeConfig.width! * 3),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        width: 1,
+                        color: Colors.grey.shade200,
+                      )),
+                    )),
+                Expanded(
+                  child: Container(
                       margin: EdgeInsets.symmetric(
                           horizontal: SizeConfig.width! * 3,
                           vertical: SizeConfig.height! * 5),
                       decoration: const BoxDecoration(
                           image: DecorationImage(
                               image: AssetImage("bose.png"), fit: BoxFit.fill),
-                          color: Colors.white
-                      ),
-                      child:Column(
+                          color: Colors.white),
+                      child: Column(
                         children: [
                           Container(
                             height: SizeConfig.height! * 5,
@@ -52,16 +49,9 @@ class _Dept_loginState extends State<Dept_login> {
                             // alignment: Alignment.centerLeft,
                           )
                         ],
-                      )
-
-                    ),
-
-                  )
+                      )),
                 ),
-
-              ],
-            )
-          )
-        ));
+              ]),
+            )));
   }
 }
